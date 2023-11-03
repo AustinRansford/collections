c3fa99771579059fad194febb6f43d580b7e8449
AustinRansford <89941018+AustinRansford@users.noreply.github.com>
2023-11-03 11:32:16 -0700

    Update CastawayTest.java

--- CastawayTest.java
+++ CastawayTest.java
@@ -27,57 +27,17 @@ public class CastawayTest
      * @return    the sum of x and y
      */
     public static void castawayTest() {
-        MyLinkedList initialList = new MyLinkedList<Castaway>();
-        MyLinkedList lastNames = new MyLinkedList<String>();
         MyLinkedList sortedList = new MyLinkedList<Castaway>();
-        Castaway gilligan = new Castaway("", "Gilligan", 72, "M");
-        Castaway grumby = new Castaway("Grumby", "Jonas", 85, "M");
-        Castaway howellT = new Castaway("Howell", "Thurson", 82, "M");
-        Castaway howellL = new Castaway("Howell", "Lovey", 96, "F");
-        Castaway grant = new Castaway("Grant", "Ginger", 90, "F");
-        Castaway hinkley = new Castaway("Hinkely", "Roy", 96, "M");
-        Castaway summers = new Castaway("Summers", "Mary Ann", 88, "F");
-        initialList.add(gilligan);
-        initialList.add(grumby);
-        initialList.add(howellT);
-        initialList.add(howellL);
-        initialList.add(grant);
-        initialList.add(hinkley);
-        initialList.add(summers);
+
+        String[] lastName = {"","Grumby","Howell", "Howell","Grant","Hinkley", "Summers"};
+        String[] firstName = {"Gilligan", "Jonas", "Thurston", "Lovey", "Ginger", "Roy", "Mary Ann"};
+        int[] score = {72, 85, 82, 96, 90, 96, 88};
+        String[] gender = {"M", "M", "M", "F", "F", "M", "F"};
+        for (int i = 0; i < lastName.length; i++) {
+            Castaway person = new Castaway(lastName[i], firstName[i], score[i], gender[i]);
+            sortedList.insertSorted(person);
+        }
+        System.out.println(sortedList);
         
-        lastNames.insertSorted(gilligan.getLastName());
-        lastNames.insertSorted(grumby.getLastName());
-        lastNames.insertSorted(howellT.getLastName());
-        lastNames.insertSorted(howellL.getLastName());
-        lastNames.insertSorted(grant.getLastName());
-        lastNames.insertSorted(hinkley.getLastName());
-        lastNames.insertSorted(summers.getLastName());
-        System.out.println(lastNames);
-        //how can I adjust for same last name 
-        while(lastNames.size()!= 0) {
-            int currentSize = lastNames.size();
-            for(int j = 0; j < initialList.size(); j++) {
-                if (lastNames.get(0).compareTo(initialList.get(j).getLastName()) == 0){
-                    sortedList.add(initialList.get(j));
-                    initialList.remove(j);
-                    lastNames.remove(0);
-                    break;
-                }
-            }
-            if (currentSize == lastNames.size()){
-                throw new NoSuchElementException(); //nothing was removed in a pass
-            }
-        }
-        //this will sort by last name then I just need to check 
-        //if any last names are the same then swap accordingly
-        for (int i = 0; i < sortedList.size() - 1; i++) {
-            if (sortedList.get(i).getLastName().equals(sortedList.get(i+1).getLastName())) {
-                if (sortedList.get(i).getFirstName().compareTo(sortedList.get(i+1).getFirstName()) == -1) { 
-                    Castaway temp= sortedList.get(i);
-                    sortedList.set(i, sortedList.get(i + 1));
-                    sortedList.set(i+1, temp);
-                }
-            }
-        }
     }
 }
