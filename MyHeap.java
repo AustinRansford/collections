3cfc7d266e1a463320e99b1478832aefbdab0644
AustinRansford <aransford2024@chadwickschool.org>
2023-12-05 12:26:15 -0800

    created heap
    ++++++++++

---
+++ MyHeap.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class MyHeap here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyHeap<E>
+{
+    // instance variables - replace the example below with your own
+    private E[] heapArray = (E[])new Object[100];
+    
+
+    /**
+     * Constructor for objects of class MyHeap
+     */
+    public MyHeap()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
